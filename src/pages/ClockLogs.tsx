fe245d09fd057d3dd9b2d609d17df90751627367
megumi-seki <seki.megumi.v0@tufs.ac.jp>
2025-08-11 15:35:40 +0900

    make clock log page base

--- src/pages/ClockLogs.tsx
+++ src/pages/ClockLogs.tsx
@@ -1,7 +1,12 @@
 const ClockLogs = () => {
   const pageContent = (
     <div>
-      <h1>勤怠ログページ</h1>
+      <h3>〇〇日のタイムレコーダー記録</h3>
+      <div>
+        <span>シフトと比較</span>
+        <span>役職色付け</span>
+      </div>
+      <div>グラフ</div>
     </div>
   );
 
