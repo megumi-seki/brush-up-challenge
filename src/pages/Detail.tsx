c5795b8b28fec0ff5e70b79b4217ed826bb5cfeb
megumi-seki <seki.megumi.v0@tufs.ac.jp>
2025-08-11 20:28:42 +0900

    remove unused import

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 type Employee = {
   id: string;
